4435be2632eaf2994a1b258adb40da1858752538
TGFRT <68918738+TGFRT@users.noreply.github.com>
2024-10-02 20:04:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,15 @@
 import streamlit as st
+
+# Accede a los secretos
+google_secrets = st.secrets["google"]
+
+# Usa las credenciales
+from google.oauth2 import service_account
 import gspread
-from google.oauth2.service_account import Credentials
-import toml
 
-# Cargar las credenciales desde secrets.toml
-secrets = toml.load("secrets.toml")
-google_creds = secrets['google']
+credentials = service_account.Credentials.from_service_account_info(google_secrets)
+gc = gspread.authorize(credentials)
 
-# Configura las credenciales
-creds = Credentials.from_service_account_info(google_creds)
-
-# Autenticación con Google Sheets
-gc = gspread.authorize(creds)
-
-# ID de la hoja de cálculo
+# Ahora puedes usar `gc` para interactuar con Google Sheets
 spreadsheet_id = '1prl2yPkqMDeUdA7Gi0NoSz6aqnORsRMDNou36yFbpbA'
-
-# Abre la hoja de cálculo por su ID
 spreadsheet = gc.open_by_key(spreadsheet_id)
-
-# Selecciona la primera hoja
-worksheet = spreadsheet.sheet1  # Puedes cambiar esto si necesitas otra hoja
-
-# Lee datos de la hoja
-data = worksheet.get_all_records()
-
-# Muestra los datos en Streamlit
-st.title("Registros de Usuarios")
-st.write(data)
